0e2e37b15762aeeb41293ee6d85a50faa4368f54
sk1F25 <sk1fplaygames@gmail.com>
2023-07-27 20:38:25 +0300

    1.5 create

--- script.js
+++ script.js
@@ -2,61 +2,59 @@ const readMoreButton = document.querySelector('.btn__read-more');
 const readMoreButtonText = document.querySelector('.btn__read-more-text');
 
 let swiper = 0;
-let resizeTimeout = 0;
 
 function swiperInitialize() {
-    if (!swiper) {
-        console.log('Swiper init');
-        swiper = new Swiper('.swiper', {
-            direction: 'horizontal',
-            loop: true,
-            slidesPerView: 'auto',
-            spaceBetween: 16,
-            observer: true,
-            observeParents: true,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-        });
-    }
+    swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 'auto',
+        spaceBetween: 16,
+        observer: true,
+        observeParents: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
 }
 
 function swiperOff() {
-    console.log('swiper off');
     if (swiper) {
-        swiper.disable();
         swiper.destroy();
         swiper = 0;
     }
 }
 
-function handleResize() {
-    clearTimeout(resizeTimeout);
-    resizeTimeout = setTimeout(function () {
-        if (window.innerWidth < 767) {
-            swiperInitialize();
-        } else {
-            swiperOff();
-        }
-    }, 500); // Задержка времени перед инициализацией Swiper
-}
+window.onload = function () {
+    if (window.matchMedia('(max-width: 767px)').matches) {
+        swiperInitialize();
+    } else {
+        swiperOff();
+    }
+};
 
-window.addEventListener('resize', handleResize);
-
-// Вызываем функцию инициализации Swiper при загрузке страницы
-handleResize();
+window.addEventListener('resize', function () {
+    if (window.innerWidth < 767) {
+        swiperInitialize()
+    } else {
+        swiperOff()
+    }
+});
 
 readMoreButton.addEventListener('click', function () {
-    const brandBlock = document.querySelector('.brand-block');
+    const brandBlock = document.querySelector('.brand-list');
+    const readMoreIcon = document.querySelector('.btn__read-more-icon')
+
     switch (readMoreButtonText.textContent) {
         case 'Показать все':
             readMoreButtonText.textContent = 'Скрыть';
-            brandBlock.classList.add('brand-block--extended');
+            brandBlock.classList.add('brand-list--extended');
+            readMoreIcon.style.transform = 'rotate(180deg)';
             break;
         case 'Скрыть':
             readMoreButtonText.textContent = 'Показать все';
-            brandBlock.classList.remove('brand-block--extended');
+            brandBlock.classList.remove('brand-list--extended');
+            readMoreIcon.style.transform = 'rotate(0deg)';
             break;
     }
 });
